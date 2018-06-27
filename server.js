a4e22d204e1701dc2149105f631f8fe2ce7b6cb6
Frank Hernandez <fhern077@fiu.edu>
2018-06-26 20:38:04 -0400

    added base port stuff

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = PROCESS.env.port || 5000;
+
+app.listen('/', (req, res) => {
+    res.send('hello world')
+});
+app.use(PORT, () => {
+    console.log(`Hey there guise I'm on port ${PORT}`)
+});
