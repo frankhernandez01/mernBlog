817caadae7cab2f22c584b902a89ad83f2e93d9a
Frank Hernandez <frankee9900@gmail.com>
2018-06-26 21:52:06 -0400

    added more base struct

---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const post = require('./post');
+const profile = require('./profile');
+const user = require ('./user');
+
+module.exports=post;
+module.exports=profile;
+module.exports=user;
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { posts, profile, user } = require('./routes/api');
 const app = express();
 const PORT = process.env.port || 5001;
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-});
+//DB Config 
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB 
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+//Base End Point
+app.get('/', (req, res) => { res.send('hello world') });
+
+//Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+//Listen on the port
 app.use(PORT, () => {
     console.log(`Hey there guise I'm on ports ${PORT}`)
 });
