e96cd24b8ecdbdf771d7da30a2a349429fcc58b7
Frank Hernandez <fhern077@fiu.edu>
2018-06-26 20:53:42 -0400

    user check #2

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.listen('/', (req, res) => {
     res.send('hello world')
 });
 app.use(PORT, () => {
-    console.log(`Hey there guise I'm on ports ${PORT}`)
+    console.log(`Hey there guise I'm on port ${PORT}`)
 });
 
